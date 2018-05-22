0f3c788ccf16c146d767480774cb35fb1adf05b1
Floris Barthel <floris.barthel@gmail.com>
2018-05-22 10:57:43 -0400

    nest

--- R/nested-list-example.R
+++ R/nested-list-example.R
@@ -1,10 +1,67 @@
 library(tidyverse)
 library(stringi)
 
-df = data.frame(client  = c(rep("A",4), rep("B",4)),
-                case    = rep(c("P","R"),4),
-                file    = stri_rand_strings(8, 6, '[A-Z]'))
+df = data.frame(patient  = c(rep("A",4), rep("B",4)),
+                sample    = rep(c("P","R"),4),
+                file      = stri_rand_strings(8, 6, '[A-Z]'))
 
-df %>%
-  split(.$patient) %>%
-  map(split, .$sample)
+dfout <- df %>% group_by(patient, sample) %>% 
+  summarize(files=list(map(file, ~list(file=.x)))) %>% 
+  summarize(samples=list(map2(sample, files, ~list(samples=.x, files=.y))))
+
+# [
+#   {
+#     "patient" : "A",
+#     "samples" : [
+#       {
+#         "sample" : "P",
+#         "files" : [
+#           {
+#             "file" : "ZZEVYQ"
+#           },
+#           {
+#             "file" : "XRYBUE"
+#           }
+#         ] 
+#       },
+#       {
+#         "sample" : "R",
+#         "files" : [
+#           {
+#             "file" : "KIUXRU"
+#           },
+#           {
+#             "file" : "ZCHBKN"
+#           }
+#         ]
+#       }
+#     ]
+#   },
+#   {
+#     "patient" : "B",
+#     "samples" : [
+#       {
+#         "sample" : "P",
+#         "files" : [
+#           {
+#             "file" : "WZYAPM"
+#           },
+#           {
+#             "file" : "CYEJCK"
+#           }
+#           ] 
+#       },
+#       {
+#         "sample" : "R",
+#         "files" : [
+#           {
+#             "file" : "EKDFYT"
+#           },
+#           {
+#             "file" : "XFAYXX"
+#           }
+#         ]
+#       }
+#     ]
+#   }
+# ]
