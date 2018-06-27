a0a6b6e766929589d4695a8a18740a3d8b3a1206
Floris Barthel <floris.barthel@gmail.com>
2018-06-27 12:14:46 -0400

    update germline var

--- bin/select-germline-variants.sh
+++ bin/select-germline-variants.sh
@@ -3,7 +3,17 @@
 
 module load bcftools
 
-bcftools norm -m - -o "/projects/verhaak-lab/verhaak_ref/gatk-cnv/af-only-gnomad.raw.sites.b37.norm.vcf.gz" -O z "/projects/verhaak-lab/verhaak_ref/gatk-legacy-bundles/Mutect2/af-only-gnomad.raw.sites.b37.vcf.gz"
+bcftools norm \
+	-f "/projects/verhaak-lab/verhaak_ref/gatk-legacy-bundles/b37/human_g1k_v37_decoy.fasta" \
+	-m- \
+	-o "/projects/verhaak-lab/verhaak_ref/gatk-cnv/af-only-gnomad.raw.sites.b37.norm.vcf.gz" \
+	-O z \
+	--threads 6 \
+	"/projects/verhaak-lab/verhaak_ref/gatk-legacy-bundles/Mutect2/af-only-gnomad.raw.sites.b37.vcf.gz"
+
+gatk IndexFeatureFile \
+	-F "/projects/verhaak-lab/verhaak_ref/gatk-cnv/af-only-gnomad.raw.sites.b37.norm.vcf.gz"
+
 gatk SelectVariants \
 	-V  "/projects/verhaak-lab/verhaak_ref/gatk-cnv/af-only-gnomad.raw.sites.b37.norm.vcf.gz" \
 	-L 1 \
