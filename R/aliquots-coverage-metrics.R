03308e913f6e2de1716c9a2954c3ed33efe154a3
Kevin J <kevin.c.johnson.gr@dartmouth.edu>
2018-11-08 11:20:06 -0500

    updated cumulative cov

--- R/aliquots-coverage-metrics.R
+++ R/aliquots-coverage-metrics.R
@@ -50,20 +50,5 @@ glass_samples_cumulative_cov = glass_cov %>%
   select(aliquot_id = sample_id, coverage, high_quality_coverage_count, cumulative_coverage)
 
 # Write output as one table or a table for each file:
-write.table(glass_samples_cumulative_cov, file = "/Users/johnsk/Documents/glass-cumulative-coverage.txt", sep="\t", row.names = F, col.names = T, quote = F)
+write.table(glass_samples_cumulative_cov, file = "/Users/johnsk/Documents/Life-History/GLASS-WG/data/ref/glass-cumulative-coverage.txt", sep="\t", row.names = F, col.names = T, quote = F)
 
-
-cov_dat = mclapply(files, function(f){
-  dat = tryCatch(read.delim(f,as.is=T, header=T, row.names = NULL, skip = 10), error=function(e) e)
-  if(inherits(dat,'error')) {
-    message(f, '\n', dat, '\n')
-    return()
-  }
-  # Truncate the file name to just the sample_id.
-  dat = dat %>%
-    mutate(sample_id = gsub(".WgsMetrics.txt", "", basename(f))) # %>%  
-  #    filter(coverage!="0") # Filter out those bases with `0` coverage.
-  
-  return(dat)
-  
-}, mc.cores=20)
