43a4d8d2031339b662e688a6f20c217e6ac660dd
Floris Barthel <floris.barthel@gmail.com>
2019-01-18 09:25:51 -0500

    Silver set

--- sql/selected_tumor_pairs_silver.sql
+++ sql/selected_tumor_pairs_silver.sql
@@ -1,5 +1,5 @@
 WITH
-selected_tumor_pairs AS
+silver_set AS
 (
 	SELECT
 		tumor_pair_barcode,
@@ -13,6 +13,29 @@ selected_tumor_pairs AS
 	WHERE
 		comparison_type = 'longitudinal' AND
 		sample_type_b <> 'M1' AND
-		b1.coverage_exclusion = 'allow' AND b2.coverage_exclusion = 'allow' 
+		b1.coverage_exclusion = 'allow' AND b2.coverage_exclusion = 'allow'
+),
+gold_set AS
+(
+	SELECT
+		tumor_pair_barcode,
+		case_barcode,
+		tumor_barcode_a,
+		tumor_barcode_b,
+		row_number() OVER (PARTITION BY case_barcode ORDER BY surgical_interval_mo DESC, portion_a ASC, portion_b ASC, substring(tumor_pair_barcode from 27 for 3) ASC) AS priority
+	FROM analysis.tumor_pairs ps
+	LEFT JOIN analysis.blocklist b1 ON b1.aliquot_barcode = ps.tumor_barcode_a
+	LEFT JOIN analysis.blocklist b2 ON b2.aliquot_barcode = ps.tumor_barcode_b
+	WHERE
+		comparison_type = 'longitudinal' AND
+		sample_type_b <> 'M1' AND
+		b1.coverage_exclusion = 'allow' AND b2.coverage_exclusion = 'allow' AND
+		b1.cnv_exclusion IN ('allow','review') AND b2.cnv_exclusion IN ('allow','review')
 )
-SELECT * FROM selected_tumor_pairs WHERE priority = 1
+SELECT *
+FROM gold_set WHERE priority = 1
+
+EXCEPT
+
+SELECT *
+FROM silver_set WHERE priority = 1
