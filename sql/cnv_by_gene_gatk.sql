408d38a6dd221436a625e782f9491274e950a18c
Floris Barthel <floris.barthel@gmail.com>
2019-01-18 09:26:07 -0500

    Gene level CN calls

--- sql/cnv_by_gene_gatk.sql
+++ sql/cnv_by_gene_gatk.sql
@@ -9,7 +9,7 @@ Perform gene level copy number calling using GATK CallSegments segmentation file
 WITH
 selected_genes AS
 (
-	SELECT * FROM ref.genes
+	SELECT * FROM ref.genes WHERE gene_symbol IN ('CDKN2A','EGFR')
 ),
 gene_seg_intersect AS
 (
@@ -25,9 +25,32 @@ gene_sample_call AS
     GROUP BY aliquot_barcode, gene_symbol
 )
 SELECT
-	*,
-	(CASE WHEN corrected_cn < -1 THEN -1 WHEN corrected_cn > 1 THEN 1 ELSE 0 END)::smallint cn_call
-FROM gene_sample_call
---WHERE aliquot_barcode LIKE 'GLSS-SM-R109-%'
---AND gene_symbol IN ('PTEN','CDKN2A')
---ORDER BY corrected_cn ASC 
+	gc.aliquot_barcode,
+	gc.gene_symbol,
+	gc.corrected_cn,
+	(CASE
+	 WHEN gc.corrected_cn < -1 THEN '-'
+	 WHEN gc.corrected_cn > 1 THEN '+'
+	 ELSE '0' END) hard_thresholded_call,
+	(CASE
+	 WHEN 2^gc.corrected_cn >= 0.9 AND 2^gc.corrected_cn <= 1.1 THEN '0'
+	 WHEN (2^gc.corrected_cn - fwmean) < -2.0 * fwsd THEN '-'
+	 WHEN (2^gc.corrected_cn - fwmean) > 2.0 * fwsd THEN '+'
+	 ELSE '0'
+	 END) zscore_call_2sd,
+	titan_call,
+	(2^gc.corrected_cn - fwmean) / fwsd AS zscore,
+	(CASE
+	 WHEN 2^gc.corrected_cn >= 0.9 AND 2^gc.corrected_cn <= 1.1 THEN 0
+	 WHEN 2^gc.corrected_cn < loss_wmean - 2 * loss_wsd THEN -2
+	 WHEN (2^gc.corrected_cn - fwmean) < -2.0 * fwsd THEN -1
+	 WHEN 2^gc.corrected_cn > gain_wmean + 2 * gain_wsd THEN 2
+	 WHEN (2^gc.corrected_cn - fwmean) > 2.0 * fwsd THEN 1
+	 ELSE 0
+	 END) hlvl_call
+FROM gene_sample_call gc
+LEFT JOIN analysis.filtered_seg_wmean_wsd rs ON rs.aliquot_barcode = gc.aliquot_barcode
+LEFT JOIN analysis.pairs tp ON tp.tumor_barcode = gc.aliquot_barcode
+LEFT JOIN analysis.cnv_by_gene cng ON cng.pair_barcode = tp.pair_barcode AND cng.gene_symbol = gc.gene_symbol
+LEFT JOIN analysis.taylor_aneuploidy_v2 ta ON ta.aliquot_barcode = gc.aliquot_barcode
+WHERE fwsd > 0
