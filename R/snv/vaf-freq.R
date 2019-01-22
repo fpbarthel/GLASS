dfc4545b019bbe33f0938fcfed798c9eb959668c
Floris Barthel <floris.barthel@gmail.com>
2019-01-22 15:05:27 -0500

    Cleanup

--- R/snv/vaf-freq.R
+++ R/snv/vaf-freq.R
@@ -7,14 +7,15 @@ library(RColorBrewer)
 con <- DBI::dbConnect(odbc::odbc(), "VerhaakDB")
 vaf_res <- dbGetQuery(con, read_file('sql/vaf_compare.sql'))
 
-ggplot(vaf_res, aes(vaf_a, vaf_b)) +
+g <-
+  ggplot(vaf_res, aes(vaf_a, vaf_b)) +
   geom_point(aes(color=variant_classification)) + 
   geom_abline(slope=1, alpha=0.2, linetype=2) +
   labs(x="Primary VAF", y="Optimal recurrence VAF", color = "Variant Classification") +
   coord_cartesian(xlim = c(0,1), ylim = c(0,1)) +
   theme_bw(base_size = 18) +
   theme(axis.text=element_text(size=10)) +
-  facet_wrap(~gene_symbol) + 
+  facet_wrap(~gene_symbol, ncol = 3) + 
   scale_color_manual(values=c("5'Flank" = brewer.pal(9, "Paired")[9],
                               "Frame_Shift_Del" = brewer.pal(7, "Paired")[1],
                               "Frame_Shift_Ins" = brewer.pal(7, "Paired")[2],
@@ -24,3 +25,7 @@ ggplot(vaf_res, aes(vaf_a, vaf_b)) +
                               "Nonsense_Mutation" = brewer.pal(7, "Paired")[6],
                               "Splice_Site" = brewer.pal(9, "Paired")[7],
                               "Translation_Start_Site" = brewer.pal(9, "Paired")[8]))
+
+pdf(file = "~/The Jackson Laboratory/GLASS - Documents/Figure 1/suppl_vaf.pdf", width = 10, height = 9)
+plot(g)
+dev.off()
--- sql/c710_by_pair.sql
+++
@@ -1,4 +0,0 @@
-SELECT tumor_pair_barcode, c1.c710 AS pri_c710, c2.c710 AS rec_c710
-FROM analysis.silver_set ss
-LEFT JOIN analysis.c710 c1 ON c1.aliquot_barcode = ss.tumor_barcode_a
-LEFT JOIN analysis.c710 c2 ON c2.aliquot_barcode = ss.tumor_barcode_b
