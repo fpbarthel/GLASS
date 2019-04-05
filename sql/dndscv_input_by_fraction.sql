4a0d5e8f635f090f2823124511bcdabb3f18138b
Floris Barthel <floris.barthel@gmail.com>
2019-04-05 11:47:51 -0400

    dNdSCV input SQL

--- sql/dndscv_input_by_fraction.sql
+++ sql/dndscv_input_by_fraction.sql
@@ -2,39 +2,22 @@
 Prepare input for use with dNdS CV
 Remove contiguous sites using EXISTS
 */
-WITH selected_tumor_pairs AS
-(
-    SELECT * FROM analysis.silver_set
-)
-
-SELECT DISTINCT -- remove duplicate entries
-    gt.case_barcode,
-    gt.chrom::character varying(2),
-    lower(gt.pos) AS pos,
-    snvs.ref,
-    gt.alt AS mut,
-    na.recurrence_evolution AS evolution,
-    su.idh_codel_subtype AS subtype,
+SELECT
+  tp.case_barcode,
+  pgeno.chrom,
+  lower(pgeno.pos) AS pos,
+  pgeno.ref,
+  pgeno.alt AS mut,
+  st.idh_codel_subtype AS subtype,
     (CASE WHEN mutect2_call_a AND mutect2_call_b     THEN 'S'
           WHEN mutect2_call_a AND NOT mutect2_call_b THEN 'P'
           WHEN mutect2_call_b AND NOT mutect2_call_a THEN 'R' END) AS fraction
-    /*(CASE WHEN vaf_corrected_call_a AND vaf_corrected_call_b     THEN 'S'
-          WHEN vaf_corrected_call_a AND NOT vaf_corrected_call_b THEN 'P'
-          WHEN vaf_corrected_call_b AND NOT vaf_corrected_call_a THEN 'R' END) AS fraction*/
-    /*(CASE WHEN gt.alt_count_a > 0 AND  gt.alt_count_b > 0    THEN 'S'
-          WHEN gt.alt_count_a > 0 AND NOT gt.alt_count_b > 0 THEN 'P'
-          WHEN gt.alt_count_b > 0 AND NOT gt.alt_count_a > 0 THEN 'R' END) AS fraction*/
-FROM analysis.master_genotype_comparison gt
-INNER JOIN selected_tumor_pairs stp ON stp.tumor_pair_barcode = gt.tumor_pair_barcode 
-LEFT JOIN analysis.snvs ON snvs.chrom = gt.chrom AND snvs.pos = gt.pos AND snvs.alt = gt.alt
-LEFT JOIN analysis.neutrality_tumor_pairs na ON na.tumor_pair_barcode = gt.tumor_pair_barcode
-LEFT JOIN clinical.surgeries su ON su.sample_barcode = substring(gt.tumor_pair_barcode from 1 for 15)
+FROM variants.pgeno
+--LEFT JOIN variants.anno ON lower(anno.pos) = lower(pgeno.pos) - 1
+INNER JOIN analysis.silver_set tp ON tp.tumor_pair_barcode = pgeno.tumor_pair_barcode
+LEFT JOIN clinical.subtypes st ON st.case_barcode = pgeno.case_barcode
 WHERE
-    stp.priority = 1 AND
-    (mutect2_call_a OR mutect2_call_b) AND
-    --(gt.alt_count_a + gt.ref_count_a) >= 5 AND 
-    --(gt.alt_count_b + gt.ref_count_b) >= 5 AND
-    (gt.alt_count_a > 0 OR gt.alt_count_b > 0) AND
-    NOT EXISTS (SELECT 1 FROM analysis.snvs snvs WHERE snvs.chrom = gt.chrom AND lower(snvs.pos) = lower(gt.pos) - 1)
+    (mutect2_call_a OR mutect2_call_b) --AND anno.pos IS NULL
+--LIMIT 100000
     
 -- END --
--- sql/dndscv_input_by_sample.sql
+++ sql/dndscv_input_by_sample.sql
@@ -6,29 +6,29 @@ Modified for per sample analysis
 WITH
 selected_aliquots AS
 (
-	SELECT tumor_barcode_a AS aliquot_barcode, 'P' AS sample_type FROM analysis.silver_set
-	UNION
-	SELECT tumor_barcode_b AS aliquot_barcode, 'R' AS sample_type FROM analysis.silver_set
+    SELECT tumor_barcode_a AS aliquot_barcode, 'P' AS sample_type FROM analysis.silver_set
+    UNION
+    SELECT tumor_barcode_b AS aliquot_barcode, 'R' AS sample_type FROM analysis.silver_set
 )
 
 SELECT DISTINCT -- remove duplicate entries
     gt.aliquot_barcode,
     gt.chrom::character varying(2),
     lower(gt.pos) AS pos,
-    snvs.ref,
+    anno.ref,
     gt.alt AS mut,
     sample_type,
-	evolution,
-	idh_codel_subtype AS subtype
-FROM analysis.genotypes gt
+    --evolution,
+    idh_codel_subtype AS subtype
+FROM variants.pgeno gt
 INNER JOIN selected_aliquots sa ON sa.aliquot_barcode = gt.aliquot_barcode 
-LEFT JOIN analysis.snvs ON snvs.chrom = gt.chrom AND snvs.pos = gt.pos AND snvs.alt = gt.alt
-LEFT JOIN analysis.neutrality_aliquots na ON na.aliquot_barcode = gt.aliquot_barcode
-LEFT JOIN clinical.surgeries su ON su.sample_barcode = substring(gt.aliquot_barcode from 1 for 15)
+LEFT JOIN variants.anno ON anno.chrom = gt.chrom AND anno.pos = gt.pos AND anno.alt = gt.alt
+--LEFT JOIN analysis.neutrality_aliquots na ON na.aliquot_barcode = gt.aliquot_barcode
+LEFT JOIN clinical.subtypes su ON su.case_barcode = substring(gt.aliquot_barcode from 1 for 12)
 WHERE
-    mutect2_call AND
+    ssm2_pass_call AND
     --(gt.alt_count + gt.ref_count) >= 30 AND 
-    gt.alt_count > 0 AND
-    NOT EXISTS (SELECT 1 FROM analysis.snvs snvs WHERE snvs.chrom = gt.chrom AND lower(snvs.pos) = lower(gt.pos) - 1)
+    gt.ad_alt > 0 AND
+    NOT EXISTS (SELECT 1 FROM variants.anno WHERE anno.chrom = gt.chrom AND lower(anno.pos) = lower(gt.pos) - 1)
     
 -- END --
