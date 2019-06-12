3b2b9b5caaa01d918f5ad000af473952f230fb02
Fred Varn <Frederick.Varn@jax.org>
2019-06-12 11:15:40 -0400

    removing scripts that have been archived

--- R/mutsigs/mutpattern_runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#PBS -l nodes=1:ppn=12,walltime=70:00:00,mem=72gb
-
-#Rscript /projects/varnf/GLASS/GLASS/R/mutsigs/mutationalpatterns.r
-#Rscript /projects/varnf/GLASS/GLASS/R/mutsigs/individual_mutsig_heatmap.r
-Rscript /projects/varnf/GLASS/GLASS/R/mutsigs/primary_recur_sig_comparison.r
